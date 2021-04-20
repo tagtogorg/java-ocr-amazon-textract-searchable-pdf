892c328fcbddc86ced31789b9d7807e926377a1c
Juan Miguel Cejuela <i@juanmi.rocks>
2021-04-20 15:29:58 +0200

    wip demo

--- src/SearchablePDF/src/main/java/DemoTagtogOcr.java
+++ src/SearchablePDF/src/main/java/DemoTagtogOcr.java
@@ -1,23 +1,15 @@
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Stream;
-
-import com.amazonaws.http.HttpResponse;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -25,16 +17,20 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
 
 // https://eu-west-1.console.aws.amazon.com/textract/home?region=eu-west-1#/demo
 // https://docs.aws.amazon.com/textract/latest/dg/getting-started.html
+// https://github.com/aws-samples/amazon-textract-searchable-pdf
 public class DemoTagtogOcr {
 
     private static DemoPdfFromLocalPdf localPdf = new DemoPdfFromLocalPdf();
@@ -47,7 +43,7 @@ public class DemoTagtogOcr {
     private static String TAGTOG_PASSWORD = Optional.ofNullable(System.getenv("TAGTOG_PASSWORD"))
             .orElseThrow(() -> new IllegalArgumentException("You must define the envar: TAGTOG_PASSWORD"));
 
-    private static CloseableHttpClient httpClient;
+    private static CloseableHttpClient httpClient = null;
 
     private static String ttProjectOwner;
     private static String ttProjectName;
@@ -55,19 +51,23 @@ public class DemoTagtogOcr {
 
     private static String DOCS_API_URL = TAGTOG_DOMAIN + "/-api/documents/v1";
 
-    public static void main(String args[]) throws IOException {
-        // https://mkyong.com/java/apache-httpclient-examples/
-        CredentialsProvider credsProvider = new BasicCredentialsProvider();
-        {
+    public static void main(String args[]) throws Exception {
+        try {
+            // https://github.com/apache/httpcomponents-client/blob/4.5.x/httpclient/src/examples/org/apache/http/examples/client/ClientAuthentication.java
+            // https://mkyong.com/java/apache-httpclient-examples/
+            // https://github.com/apache/httpcomponents-client/blob/4.5.x/httpclient/src/examples/org/apache/http/examples/client/ClientPreemptiveBasicAuthentication.java
+            // https://stackoverflow.com/questions/7482523/preemptive-authentication-why
+            CredentialsProvider credsProvider = new BasicCredentialsProvider();
+
             credsProvider.setCredentials(AuthScope.ANY,
                     new UsernamePasswordCredentials(TAGTOG_USERNAME, TAGTOG_PASSWORD));
-        }
-        httpClient = HttpClientBuilder.create().setDefaultCredentialsProvider(credsProvider).build();
 
-        System.out.println(credsProvider.getCredentials(AuthScope.ANY).getPassword());
+            httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider)
+                    .setSSLContext(
+                            new SSLContextBuilder().loadTrustMaterial(null, TrustSelfSignedStrategy.INSTANCE).build())
+                    .setRedirectStrategy(new LaxRedirectStrategy()).build();
 
-        try {
-            testCredentials();
+            testCredentials(httpClient);
 
             ttProjectOwner = args[0];
             ttProjectName = args[1];
@@ -80,20 +80,25 @@ public class DemoTagtogOcr {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            credsProvider.clear();
-            httpClient.close();
+            if (httpClient != null) {
+                httpClient.close();
+            }
         }
     }
 
     public static void applyFun(Path inputPath) {
+        File outputTmpFile = null;
         try {
-            // File outputTmpFile = doOcr(inputPath.toString());
-            File outputTmpFile = new File(
-                    "/var/folders/0b/3zs8hgcn3m37_4rgg7v86ywc0000gn/T/tagtog-sample-ocr14069932450390385825.pdf");
+            outputTmpFile = doOcr(inputPath.toString()); // Hardcode path if needed when testing
+
             uploadToTagtog(outputTmpFile, inputPath.getFileName().toString());
-            // outputTmpFile.delete();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            if (outputTmpFile != null) {
+                outputTmpFile.delete();
+            }
         }
     }
 
@@ -106,21 +111,13 @@ public class DemoTagtogOcr {
         });
     }
 
-    /**
-     * References:
-     * https://stackoverflow.com/questions/6917105/java-http-client-to-upload-file-over-post
-     * https://stackoverflow.com/questions/9093237/setting-custom-file-name-in-apache-httpclient
-     * https://stackoverflow.com/questions/9907161/commons-httpclient-adding-query-string-parameters-to-get-post-request
-     *
-     * @param file
-     */
-    public static void testCredentials() throws URISyntaxException, IOException {
+    public static void testCredentials(CloseableHttpClient httpClient) throws URISyntaxException, IOException {
         String userUrl = TAGTOG_DOMAIN + "/" + TAGTOG_USERNAME;
 
         try (CloseableHttpResponse response = httpClient.execute(new HttpGet(userUrl))) {
-            System.out.println(response.getAllHeaders());
+            // Toggle when debugging
+            // Stream.of(response.getAllHeaders()).forEach(x -> System.out.println(x));
             int statusCode = response.getStatusLine().getStatusCode();
-            System.out.println(statusCode);
 
             if (statusCode != 200) {
                 throw new IllegalArgumentException(
@@ -146,7 +143,7 @@ public class DemoTagtogOcr {
 
         HttpPost post = new HttpPost(urlBuilder.build());
 
-        System.out.println(urlBuilder.build());
+        System.out.println("POST " + desiredFilename + " to:" + urlBuilder.build());
 
         HttpEntity entity = MultipartEntityBuilder.create()
                 .addPart("files", new FileBody(file, ContentType.APPLICATION_OCTET_STREAM, desiredFilename)).build();
