c2b51588a2873251dcee7592ec586ab833a478bb
Juan Miguel Cejuela <i@juanmi.rocks>
2021-04-20 17:24:51 +0200

    reorganize for clarity

--- src/SearchablePDF/src/main/java/DemoTagtogOcr.java
+++ src/SearchablePDF/src/main/java/DemoTagtogOcr.java
@@ -11,20 +11,23 @@ import java.util.function.Consumer;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 
@@ -33,49 +36,56 @@ import org.apache.http.util.EntityUtils;
 // https://github.com/aws-samples/amazon-textract-searchable-pdf
 public class DemoTagtogOcr {
 
-    private static DemoPdfFromLocalPdf localPdf = new DemoPdfFromLocalPdf();
-    private static DemoPdfFromLocalImage localImage = new DemoPdfFromLocalImage();
-
-    private static String TAGTOG_DOMAIN = Optional.ofNullable(System.getenv("TAGTOG_DOMAIN"))
+    private static final String TAGTOG_DOMAIN = Optional.ofNullable(System.getenv("TAGTOG_DOMAIN"))
             .orElse("https://tagtog.net");
-    private static String TAGTOG_USERNAME = Optional.ofNullable(System.getenv("TAGTOG_USERNAME"))
+    private static final String TAGTOG_USERNAME = Optional.ofNullable(System.getenv("TAGTOG_USERNAME"))
             .orElseThrow(() -> new IllegalArgumentException("You must define the envar: TAGTOG_USERNAME"));
-    private static String TAGTOG_PASSWORD = Optional.ofNullable(System.getenv("TAGTOG_PASSWORD"))
+    private static final String TAGTOG_PASSWORD = Optional.ofNullable(System.getenv("TAGTOG_PASSWORD"))
             .orElseThrow(() -> new IllegalArgumentException("You must define the envar: TAGTOG_PASSWORD"));
 
-    private static CloseableHttpClient httpClient = null;
+    // -------------------------------------------------------------------------
 
     private static String ttProjectOwner;
     private static String ttProjectName;
     private static String ttProjectFolder;
 
-    private static String DOCS_API_URL = TAGTOG_DOMAIN + "/-api/documents/v1";
+    private static CloseableHttpClient httpClient = null;
+
+    // -------------------------------------------------------------------------
+
+    private static final String DOCS_API_URL = TAGTOG_DOMAIN + "/-api/documents/v1";
+
+    private static final Set<String> ACCEPTED_FILES_EXTS = Set.of("pdf", "png", "jpg", "jpeg");
+
+    private static final Credentials credentials = new UsernamePasswordCredentials(TAGTOG_USERNAME, TAGTOG_PASSWORD);
+
+    // https://github.com/apache/httpcomponents-client/blob/4.5.x/httpclient/src/examples/org/apache/http/examples/client/ClientAuthentication.java
+    private static final CredentialsProvider credsProvider = new BasicCredentialsProvider();
+    static {
+        credsProvider.setCredentials(AuthScope.ANY, credentials);
+    };
+
+    // -------------------------------------------------------------------------
+
+    private static final DemoPdfFromLocalPdf localPdf = new DemoPdfFromLocalPdf();
+    private static final DemoPdfFromLocalImage localImage = new DemoPdfFromLocalImage();
+
+    // -------------------------------------------------------------------------
 
     public static void main(String args[]) throws Exception {
         try {
-            // https://github.com/apache/httpcomponents-client/blob/4.5.x/httpclient/src/examples/org/apache/http/examples/client/ClientAuthentication.java
-            // https://mkyong.com/java/apache-httpclient-examples/
-            // https://github.com/apache/httpcomponents-client/blob/4.5.x/httpclient/src/examples/org/apache/http/examples/client/ClientPreemptiveBasicAuthentication.java
-            // https://stackoverflow.com/questions/7482523/preemptive-authentication-why
-            CredentialsProvider credsProvider = new BasicCredentialsProvider();
+            initHttpClient();
 
-            credsProvider.setCredentials(AuthScope.ANY,
-                    new UsernamePasswordCredentials(TAGTOG_USERNAME, TAGTOG_PASSWORD));
-
-            httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider)
-                    .setSSLContext(
-                            new SSLContextBuilder().loadTrustMaterial(null, TrustSelfSignedStrategy.INSTANCE).build())
-                    .setRedirectStrategy(new LaxRedirectStrategy()).build();
-
-            testCredentials(httpClient);
+            testTagtogCredentials();
 
             ttProjectOwner = args[0];
             ttProjectName = args[1];
             ttProjectFolder = args[2];
 
-            String inputFilepath = args[3];
-
-            traverseTreeAndApply(inputFilepath, DemoTagtogOcr::applyFun);
+            for (int i = 3; i < args.length; i++) {
+                String inputFilepath = args[i];
+                traverseTreeAndApply(inputFilepath, DemoTagtogOcr::doOcrAndUploadToTagtog);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -86,7 +96,9 @@ public class DemoTagtogOcr {
         }
     }
 
-    public static void applyFun(Path inputPath) {
+    // -------------------------------------------------------------------------
+
+    public static void doOcrAndUploadToTagtog(Path inputPath) {
         File outputTmpFile = null;
         try {
             outputTmpFile = doOcr(inputPath.toString()); // Hardcode path if needed when testing
@@ -99,59 +111,6 @@ public class DemoTagtogOcr {
             if (outputTmpFile != null) {
                 outputTmpFile.delete();
             }
-        }
-    }
-
-    public static void traverseTreeAndApply(String inputFilepath, Consumer<Path> applyFun) throws IOException {
-        Files.walk(Paths.get(inputFilepath)).filter(p -> {
-            return Files.isRegularFile(p)
-                    && ACCEPTED_FILES_EXTS.contains(getLowercasedExtension(p.getFileName().toString()));
-        }).forEach(p -> {
-            applyFun(p);
-        });
-    }
-
-    public static void testCredentials(CloseableHttpClient httpClient) throws URISyntaxException, IOException {
-        String userUrl = TAGTOG_DOMAIN + "/" + TAGTOG_USERNAME;
-
-        try (CloseableHttpResponse response = httpClient.execute(new HttpGet(userUrl))) {
-            // Toggle when debugging
-            // Stream.of(response.getAllHeaders()).forEach(x -> System.out.println(x));
-            int statusCode = response.getStatusLine().getStatusCode();
-
-            if (statusCode != 200) {
-                throw new IllegalArgumentException(
-                        "The authentication credentials seem wrong. Received " + statusCode + " on " + userUrl);
-            }
-        }
-    }
-
-    /**
-     * References:
-     * https://stackoverflow.com/questions/6917105/java-http-client-to-upload-file-over-post
-     * https://stackoverflow.com/questions/9093237/setting-custom-file-name-in-apache-httpclient
-     * https://stackoverflow.com/questions/9907161/commons-httpclient-adding-query-string-parameters-to-get-post-request
-     *
-     * @param file
-     */
-    public static void uploadToTagtog(File file, String desiredFilename) throws URISyntaxException, IOException {
-        URIBuilder urlBuilder = new URIBuilder(DOCS_API_URL);
-        urlBuilder.setParameter("owner", ttProjectOwner) //
-                .setParameter("project", ttProjectName) //
-                .setParameter("folder", ttProjectFolder) //
-                .setParameter("output", "null");
-
-        HttpPost post = new HttpPost(urlBuilder.build());
-
-        System.out.println("POST " + desiredFilename + " to:" + urlBuilder.build());
-
-        HttpEntity entity = MultipartEntityBuilder.create()
-                .addPart("files", new FileBody(file, ContentType.APPLICATION_OCTET_STREAM, desiredFilename)).build();
-        post.setEntity(entity);
-
-        try (CloseableHttpResponse response = httpClient.execute(post)) {
-            String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-            System.out.println(response.getStatusLine() + " " + responseBody);
         }
     }
 
@@ -175,8 +134,72 @@ public class DemoTagtogOcr {
         }
     }
 
-    private static final Set<String> ACCEPTED_FILES_EXTS = Set.of("pdf", "png", "jpg", "jpeg");
+    /**
+     * References:
+     * https://stackoverflow.com/questions/6917105/java-http-client-to-upload-file-over-post
+     * https://stackoverflow.com/questions/9093237/setting-custom-file-name-in-apache-httpclient
+     * https://stackoverflow.com/questions/9907161/commons-httpclient-adding-query-string-parameters-to-get-post-request
+     *
+     * @param file            file to upload to tagtog
+     * @param desiredFilename filename that will be sent to tagtog
+     */
+    public static void uploadToTagtog(File file, String desiredFilename)
+            throws URISyntaxException, IOException, AuthenticationException {
+        URIBuilder urlBuilder = new URIBuilder(DOCS_API_URL);
+        urlBuilder.setParameter("owner", ttProjectOwner) //
+                .setParameter("project", ttProjectName) //
+                .setParameter("folder", ttProjectFolder) //
+                .setParameter("output", "null");
 
+        HttpPost post = new HttpPost(urlBuilder.build());
+
+        System.out.println("POST " + desiredFilename + " to:" + urlBuilder.build());
+
+        HttpEntity entity = MultipartEntityBuilder.create()
+                .addPart("files", new FileBody(file, ContentType.APPLICATION_OCTET_STREAM, desiredFilename)).build();
+        post.setEntity(entity);
+
+        try (CloseableHttpResponse response = httpClient.execute(doPreemptiveBasicAuth(post))) {
+            String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            System.out.println(response.getStatusLine() + " " + responseBody);
+        }
+    }
+
+    public static void testTagtogCredentials() throws URISyntaxException, IOException, AuthenticationException {
+        String userUrl = TAGTOG_DOMAIN + "/" + TAGTOG_USERNAME;
+
+        try (CloseableHttpResponse response = httpClient.execute(doPreemptiveBasicAuth(new HttpGet(userUrl)))) {
+            // Toggle when debugging
+            // Stream.of(response.getAllHeaders()).forEach(x -> System.out.println(x));
+            int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode != 200) {
+                throw new IllegalArgumentException(
+                        "The authentication credentials seem wrong. Received " + statusCode + " on " + userUrl);
+            }
+        }
+    }
+
+    public static void traverseTreeAndApply(String inputFilepath, Consumer<Path> applyFun) throws IOException {
+        Files.walk(Paths.get(inputFilepath)).filter(p -> {
+            return Files.isRegularFile(p)
+                    && ACCEPTED_FILES_EXTS.contains(getLowercasedExtension(p.getFileName().toString()));
+        }).forEach(p -> {
+            applyFun.accept(p);
+        });
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Choose the right OcrProcessor depending on the input file's extension.
+     *
+     * NOTE: this function assumes to receive compatible files only; @see
+     * ACCEPTED_FILES_EXTS.
+     *
+     * @param inputFilepath
+     * @return
+     */
     public static OcrProcessor chooseOcr(String inputFilepath) {
         OcrProcessor ocrProcessor;
         String extension = getLowercasedExtension(inputFilepath);
@@ -190,6 +213,38 @@ public class DemoTagtogOcr {
         }
 
         return ocrProcessor;
+    }
+
+    // -------------------------------------------------------------------------
+
+    private static void initHttpClient() throws Exception {
+        httpClient = HttpClients.custom() //
+                .setDefaultCredentialsProvider(credsProvider) //
+                .setSSLContext(
+                        new SSLContextBuilder().loadTrustMaterial(null, TrustSelfSignedStrategy.INSTANCE).build()) //
+                // .setRedirectStrategy(new LaxRedirectStrategy())
+                .build();
+    }
+
+    /**
+     * We do preemptive authentication for tagtog did not support pre-emptive
+     * authenticaton until 2021-04-25.
+     *
+     * This can be safely removed in a future version.
+     *
+     * References:
+     * https://github.com/apache/httpcomponents-client/blob/4.5.x/httpclient/src/examples/org/apache/http/examples/client/ClientPreemptiveBasicAuthentication.java
+     * https://stackoverflow.com/questions/7482523/preemptive-authentication-why
+     * https://stackoverflow.com/a/4328694
+     *
+     * @param httpRequest
+     * @return httpRequest with basic authentication credentials header
+     * @throws AuthenticationException
+     * @deprecated remove in future version
+     */
+    private static HttpUriRequest doPreemptiveBasicAuth(HttpUriRequest httpRequest) throws AuthenticationException {
+        httpRequest.addHeader(new BasicScheme().authenticate(credentials, httpRequest));
+        return httpRequest;
     }
 
     public static String getLowercasedExtension(String filepath) {
